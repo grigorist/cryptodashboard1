fc2fbc3eb784534c3e9f1893439df38284233879
GRACIEST <63868635+websurfers@users.noreply.github.com>
2023-03-11 00:42:50 +0500

    Add files via upload

---
+++ info.php
@@ -0,0 +1,14 @@
+<div class="col-3">
+  <div class="info">
+    <div class="search">
+    <input type="search" placeholder="Search"> 
+    <a href="#"><img src="img/Message.png" alt="Messages" width="25px"></a> <a href=""><img src="img/frame22.png" alt="Not" width="25px" style="padding-top:8px"> </a>
+    <a href="#"><img src="img/avatar.png" alt="Avatar" class="imgava" width="30px" style="padding-buttom:7px"></a>
+    </div>
+
+    <div class="convert">
+        <h3>Convert</h3>
+    </div>
+
+  </div>
+    </div>
